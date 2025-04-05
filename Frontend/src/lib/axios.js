3bd31c0350734818ceb33e2914ca7bc0ffad3a6f
Class9123 <jayjaygovind4@gmail.com>
2025-04-05 13:33:49 +0545

    Added env mode for dev and prod

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
+let url;
+if (import.meta.env.MODE === 'development') {
+  url = import.meta.env.VITE_DEVELOPMENT_API_SERVER
+  console.log('Axios development environment');
+} else if (import.meta.env.MODE === 'production') {
+  url = import.meta.env.VITE_PRODUCTION_API_SERVER
+  console.log('axios production environment');
+}
 const axio = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  baseURL:import.meta.env.VITE_API_SERVER ,
+  baseURL:url ,
 });
 
 export default axio;
--- Frontend/src/lib/socket.js
+++ Frontend/src/lib/socket.js
@@ -1,6 +1,14 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL =import.meta.env.VITE_API_SERVER;
+let url;
+if (import.meta.env.MODE === 'development') {
+  url = import.meta.env.VITE_DEVELOPMENT_API_SERVER
+  console.log('socket development environment');
+} else if (import.meta.env.MODE === 'production') {
+  url = import.meta.env.VITE_PRODUCTION_API_SERVER
+  console.log('socket production environment');
+}
+const SOCKET_SERVER_URL = url;
 const socket = io(SOCKET_SERVER_URL);
 
 export default socket;
